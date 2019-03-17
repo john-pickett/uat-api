925cc39c64375c772934c482ffe5e5ffec45e3df
John P <john.pickett@gmail.com>
2019-03-17 14:50:51 -0500

    Updated connection URI

--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-	host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres'
 });
 
