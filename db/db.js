0d1ec78544b4376bfa3170ae6076e1499a335039
John Pickett <john.pickett@maryville.com>
2019-03-20 10:00:03 -0500

    Updated db config

--- db/db.js
+++ db/db.js
@@ -1,3 +1,31 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// 	dialect: 'postgres'
+// });
+
+// const models = {
+// 	User: sequelize.import('../models/user'),
+//     Role: sequelize.import('../models/role'),
+//     Grade: sequelize.import('../models/grade'),
+//     Feature: sequelize.import('../models/feature')
+// }
+
+// // Models
+// db.users = require('../models/users.js')(sequelize, Sequelize);
+// db.roles = require('../models/roles.js')(sequelize, Sequelize);
+// db.grades = require('../models/grade.js')(sequelize, Sequelize);
+// db.features = require('../models/feature.js')(sequelize, Sequelize);
+
+// // const models = {
+// // 	User: sequelize.import('../models/user'),
+// // 	Role: sequelize.import('../models/role')
+// // }
+
+// // db.roles.HasMany(db.users);
+
+// module.exports = db;
+
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -11,15 +39,5 @@ const models = {
     Feature: sequelize.import('../models/feature')
 }
 
-// Models
-db.users = require('../models/users.js')(sequelize, Sequelize);
-db.roles = require('../models/roles.js')(sequelize, Sequelize);
 
-// const models = {
-// 	User: sequelize.import('../models/user'),
-// 	Role: sequelize.import('../models/role')
-// }
-
-// db.roles.HasMany(db.users);
-
-module.exports = db;
+module.exports = { sequelize, models };
